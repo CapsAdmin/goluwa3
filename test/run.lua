93851bb09a4dbcfe0f9ef2444f66ec02b3cefd84
Elias Hogstvedt <eliashogstvedt@gmail.com>
2025-11-10 22:45:25 +0100

    wip

--- test/run.lua
+++
@@ -1,9 +0,0 @@
-local test = require("test.gambarina")
--- Run test files
-require("test.process")
-require("test.threads")
-require("test.buffer")
-require("test.matrix")
-require("test.png_decode")
--- Report test results
-test:report()
