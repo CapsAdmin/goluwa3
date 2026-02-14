df5dabc2789e65b5526150e03a688dffc1ef51ea
CapsAdmin <eliashogstvedt@gmail.com>
2026-02-14 22:54:58 +0700

    on editor save improvements

--- .vscode/run_on_save.lua
+++ .vscode/run_on_save.lua
@@ -1,3 +1,8 @@
 local path = ...
 require("goluwa.global_environment")
+local fs = require("fs")
+local process = require("bindings.process")
+
+if process.from_id(tonumber(fs.read_file(".running_pid"))) then return end
+
 require("filewatcher").Reload(path, true)
