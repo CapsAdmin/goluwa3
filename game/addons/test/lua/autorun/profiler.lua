fa682acdf325e3d37691c8ea917aef271677d47a
CapsAdmin <eliashogstvedt@gmail.com>
2026-03-05 22:54:03 +0700

    replace the old profiler with the new one

--- game/addons/test/lua/autorun/profiler.lua
+++ game/addons/test/lua/autorun/profiler.lua
@@ -1,14 +1,14 @@
 local commands = require("commands")
-local profiler = require("profiler")
+local Profiler = require("profiler")
 local started = false
 
 commands.Add("profile", function()
 	if not started then
-		profiler.Start("test")
+		Profiler.Start("test")
 		started = true
 		logn("profiler started")
 	else
-		profiler.Stop()
+		Profiler.Stop()
 		started = false
 		logn("profiler stopped")
 	end
--- goluwa/profiler.lua
+++ goluwa/profiler.lua
@@ -1,50 +1,21 @@
-local jit_profiler = require("helpers.jit_profiler")
-local TraceTrack = require("helpers.jit_trace_track")
+local JitProfiler = require("helpers.jit_profiler")
 local system = require("system")
-local timer = require("timer")
-local profile_stop, profile_report
-local trace_tracker
 local profiler = library()
-local f
-
-local function save_progress()
-	if trace_tracker then
-		f:write(trace_tracker:GetReportProblematicTraces() .. "\n")
-	end
-
-	if profile_report then f:write(profile_report() .. "\n") end
-
-	f:flush()
-	f:seek("set", 0)
-end
+local jit_profiler
 
 function profiler.Start(id)
 	time_start = system.GetTime()
-	trace_tracker = TraceTrack.New()
-	trace_tracker:Start()
-	profile_stop, profile_report = jit_profiler.Start()
-	f = assert(io.open("logs/profiler_" .. id .. ".md", "w"))
-	timer.Repeat("debug", 1, math.huge, save_progress)
+	jit_profiler = JitProfiler.New(
+		{
+			path = "logs/jit_profile_" .. id .. ".html",
+			file_url = "vscode://file/${path}:${line}:1",
+			get_time = system.GetTime,
+		}
+	)
 end
 
 function profiler.Stop()
-	save_progress()
-
-	do -- store total time
-		f:seek("end", 0)
-		local total_time = system.GetTime() - time_start
-
-		if f then
-			f:write("## Total time: " .. string.format("%.2f", total_time) .. " seconds\n")
-		end
-	end
-
-	f:close()
-	profile_stop()
-
-	if trace_tracker then trace_tracker:Stop() end
-
-	timer.RemoveTimer("debug")
+	jit_profiler:Stop()
 end
 
 local simple_times = {}
@@ -55,18 +26,18 @@ function profiler.StartSection(name--[[#: string]])
 	simple_times[name].time = system.GetTime()
 	table.insert(simple_stack, name)
 
-	if not stop_profiler then return end
+	if not jit_profiler then return end
 
-	jit_profiler.StartSection(name)
+	jit_profiler:StartSection(name)
 end
 
 function profiler.StopSection()
 	local name = table.remove(simple_stack)
 	simple_times[name].total = simple_times[name].total + (system.GetTime() - simple_times[name].time)
 
-	if not stop_profiler then return end
+	if not jit_profiler then return end
 
-	jit_profiler.StopSection()
+	jit_profiler:StopSection()
 end
 
 function profiler.GetSimpleSections()
