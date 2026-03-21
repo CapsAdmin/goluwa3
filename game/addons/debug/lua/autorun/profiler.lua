7aa50185781de89d5d1373927cc88881d0b58f45
CapsAdmin <eliashogstvedt@gmail.com>
2026-03-21 08:55:25 +0700

    profile toggle

--- game/addons/debug/lua/autorun/profiler.lua
+++ game/addons/debug/lua/autorun/profiler.lua
@@ -1,8 +1,9 @@
 local commands = import("goluwa/commands.lua")
+local event = import("goluwa/event.lua")
 local Profiler = import("goluwa/profiler.lua")
 local started = false
 
-commands.Add("profile", function()
+local function toggle()
 	if not started then
 		Profiler.Start("test")
 		started = true
@@ -11,5 +12,17 @@ commands.Add("profile", function()
 		Profiler.Stop()
 		started = false
 		logn("profiler stopped")
+	end
+end
+
+commands.Add("profile", function()
+	toggle()
+end)
+
+event.AddListener("KeyInput", "profiler_toggle", function(key, press)
+	if not press then return end
+
+	if key == "p" then
+		toggle()
 	end
 end)
