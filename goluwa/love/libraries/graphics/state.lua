f4b8e0699ae199d690f3e7e1202de023ec4bb88a
CapsAdmin <eliashogstvedt@gmail.com>
2026-05-14 01:20:16 +0700

    fix blend issues in love2d caused by previous render2d changes

--- goluwa/love/libraries/graphics/state.lua
+++ goluwa/love/libraries/graphics/state.lua
@@ -7,6 +7,10 @@ if type(love) == "string" then love = nil end
 love = love or _G.love
 local ensure_love_depth_target_initialized = frame.Get(love).ensure_love_depth_target_initialized
 
+function love.graphics.flushBatch()
+	render2d.FlushBatches("love.graphics.flushBatch")
+end
+
 function love.graphics.setDepthMode(compare_mode, write)
 	if not compare_mode then
 		render2d.SetDepthMode("none", false)
@@ -36,50 +40,84 @@ do
 
 	function love.graphics.setBlendMode(color_mode, alpha_mode)
 		alpha_mode = alpha_mode or "alphamultiply"
-		local func = "add"
-		local srcRGB = "one"
-		local srcA = "one"
-		local dstRGB = "zero"
-		local dstA = "zero"
+		love.graphics.flushBatch()
 
-		if color_mode == "alpha" then
-			srcRGB = "one"
-			srcA = "one"
-			dstRGB = "one_minus_src_alpha"
-			dstA = "one_minus_src_alpha"
-		elseif color_mode == "multiply" or color_mode == "multiplicative" then
-			srcRGB = "dst_color"
-			srcA = "dst_alpha"
-			dstRGB = "zero"
-			dstA = "zero"
-		elseif color_mode == "subtract" or color_mode == "subtractive" then
-			func = "subtract"
-		elseif color_mode == "add" or color_mode == "additive" then
-			srcRGB = "one"
-			srcA = "zero"
-			dstRGB = "one"
-			dstA = "one"
-		elseif color_mode == "lighten" then
-			func = "max"
-		elseif color_mode == "darken" then
-			func = "min"
-		elseif color_mode == "screen" then
-			srcRGB = "one"
-			srcA = "one"
-			dstRGB = "one_minus_src_color"
-			dstA = "one_minus_src_color"
+		if color_mode == "alpha" and alpha_mode == "alphamultiply" then
+			render2d.SetBlendPreset("alpha")
+		elseif color_mode == "alpha" and alpha_mode == "premultiplied" then
+			render2d.SetBlendPreset("premultiplied")
+		elseif
+			(
+				color_mode == "add" or
+				color_mode == "additive"
+			)
+			and
+			alpha_mode == "alphamultiply"
+		then
+			render2d.SetBlendPreset("additive")
+		elseif
+			(
+				color_mode == "multiply" or
+				color_mode == "multiplicative"
+			)
+			and
+			alpha_mode == "alphamultiply"
+		then
+			render2d.SetBlendPreset("multiply")
+		elseif color_mode == "screen" and alpha_mode == "alphamultiply" then
+			render2d.SetBlendPreset("screen")
+		elseif color_mode == "replace" and alpha_mode == "alphamultiply" then
+			render2d.SetBlendMode("src_alpha", "zero", "add", "one", "zero", "add")
+		elseif color_mode == "replace" and alpha_mode == "premultiplied" then
+			render2d.SetBlendPreset("none")
 		else
-			srcRGB = "one"
-			srcA = "one"
-			dstRGB = "zero"
-			dstA = "zero"
+			local func = "add"
+			local srcRGB = "one"
+			local srcA = "one"
+			local dstRGB = "zero"
+			local dstA = "zero"
+
+			if color_mode == "alpha" then
+				srcRGB = "one"
+				srcA = "one"
+				dstRGB = "one_minus_src_alpha"
+				dstA = "one_minus_src_alpha"
+			elseif color_mode == "multiply" or color_mode == "multiplicative" then
+				srcRGB = "dst_color"
+				srcA = "dst_alpha"
+				dstRGB = "zero"
+				dstA = "zero"
+			elseif color_mode == "subtract" or color_mode == "subtractive" then
+				func = "subtract"
+			elseif color_mode == "add" or color_mode == "additive" then
+				srcRGB = "one"
+				srcA = "zero"
+				dstRGB = "one"
+				dstA = "one"
+			elseif color_mode == "lighten" then
+				func = "max"
+			elseif color_mode == "darken" then
+				func = "min"
+			elseif color_mode == "screen" then
+				srcRGB = "one"
+				srcA = "one"
+				dstRGB = "one_minus_src_color"
+				dstA = "one_minus_src_color"
+			else
+				srcRGB = "one"
+				srcA = "one"
+				dstRGB = "zero"
+				dstA = "zero"
+			end
+
+			if srcRGB == "one" and alpha_mode == "alphamultiply" then
+				srcRGB = "src_alpha"
+			end
+
+			render2d.SetBlendMode(srcRGB, dstRGB, func, srcA, dstA, func)
 		end
 
-		if srcRGB == "one" and alpha_mode == "alphamultiply" then
-			srcRGB = "src_alpha"
-		end
-
-		render2d.SetBlendMode(srcRGB, dstRGB, func, srcA, dstA, func)
+		render2d.current_blend_mode_state = render2d.GetBlendMode()
 		COLOR_MODE = color_mode
 		ALPHA_MODE = alpha_mode
 	end
