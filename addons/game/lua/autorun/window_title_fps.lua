f09017f5b12b7f850df0fd4b1edd6c4b824cae4d
Elias Hogstvedt <eliashogstvedt@gmail.com>
2026-05-30 10:14:34 +0700

    better window title fps

--- addons/game/lua/autorun/window_title_fps.lua
+++ addons/game/lua/autorun/window_title_fps.lua
@@ -1,8 +1,19 @@
 local event = import("goluwa/event.lua")
 local system = import("goluwa/system.lua")
+local accumulated_time = 0
+local accumulated_frames = 0
 
 event.AddListener("Update", "window_title", function(dt)
-	if wait(1) then
-		system.GetWindow():SetTitle("FPS: " .. math.round(1 / system.GetFrameTime()))
+	accumulated_time = accumulated_time + dt
+	accumulated_frames = accumulated_frames + 1
+
+	if accumulated_time >= 1 and accumulated_frames > 0 then
+		local average_frame_time = accumulated_time / accumulated_frames
+		local average_fps = accumulated_frames / accumulated_time
+		system.GetWindow():SetTitle(
+			string.format("FPS: %d | avg: %.1f ms", math.round(average_fps), average_frame_time * 1000)
+		)
+		accumulated_time = 0
+		accumulated_frames = 0
 	end
 end)
